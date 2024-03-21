e262be363bacb9e68d889fff6f95e6df63eded0a
AdiPangestu2005 <adip180322@gmail.com>
2024-03-21 15:41:09 +0700

    first commit

---
+++ contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Adi Pangestu,
+            Saya tinggal di jatiwaringin,Kota Bekasi,Jawa Barat. 
+            olah raga yang saya sukai adalah 
+            Bulutangkis"; 
+    } 
+} 
